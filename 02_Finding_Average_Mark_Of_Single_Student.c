41fae79b68a3e4b764306c1e06fa4c9ce6beb419
chiragkiranshahdev <chiragkiranshahshah@gmail.com>
2026-08-18 13:55:27 +0530

    Add: Average marks of single student program

---
+++ 02_Finding_Average_Mark_Of_Single_Student.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int s1 = 80, s2 = 70, s3 = 60, s4 = 66, s5 = 70;
+    float Avg;
+    Avg = (s1+s2+s3+s4+s5)*100/500;
+    printf("Average is : %.2f\n ", Avg);
+    return 0;
+}
